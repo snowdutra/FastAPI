4bc9db03684a472136398cf5fc7e13a7d380b741
Gustavo <gusdutratelles@icloud.com>
2025-05-26 08:07:16 -0300

    update files

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from datetime import datetime
-
 
 app = FastAPI()
 
@@ -19,28 +18,32 @@ Base = declarative_base()
 class Author(Base):
 
     __tablename__ = 'author'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
+
+
 
 class Post(Base):
 
     __tablename__ = 'post'
+
     id = Column(Integer, primary_key=True)
     title = Column(String(255))
     created = Column(DateTime)
-    author_id = Column(Integer, ForeignKey('author.id', ondelete='CASCADE'))
-    
-
-    author = relationship("Author")
+    authorid = Column(Integer, ForeignKey('author.id', ondelete='CASCADE'))
+    author = relationship('Author')
 
 
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
-@app.post("/author")
 
+@app.post("/authors")
 def create_author(name: str):
+
     if not name:
-        return JSONResponse(content={"error": "Name is required"}, status_code=400)
+        return JSONResponse(content={'error': 'Name is required'}, status_code=400)
     
     try:
         author = Author(name=name)
@@ -48,61 +51,48 @@ def create_author(name: str):
         session.commit()
     except Exception as e:
         session.rollback()
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        return JSONResponse(content={'error': str(e)}, status_code=500)
 
-    return JSONResponse(content={"id": author.id, "name": author.name}, status_code=201)
+    return JSONResponse(content={'id': author.id, 'name': author.name}, status_code=201)
 
-
-@app.put("/author")
+@app.put("/authors")
 def put_author(id: int, name: str):
-
     if not id:
-        return JSONResponse(content={"error": "ID is required"}, status_code=400)
+        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
     if not name:
-        return JSONResponse(content={"error": "Name is required"}, status_code=400)
+        return JSONResponse(content={'error': 'Name is required'}, status_code=400)
     
     try:
-        author = session.query(Author).filter(Author.id == id).first()
+        author = session.query(Author).filter_by(id=id).first()
+        if not author:
+            return JSONResponse(content={'error': 'Author not found'}, status_code=404)
         author.name = name
         session.commit()
     except Exception as e:
         session.rollback()
-        return JSONResponse(content={"error": str(e)}, status_code=500)
-    
+        return JSONResponse(content={'error': str(e)}, status_code=500)
 
-    return JSONResponse(content={"id": author.id, "name": author.name}, status_code=200)
+    return JSONResponse(content={'id': author.id, 'name': author.name}, status_code=200)
 
-@app.delete("/author")
+
+@app.delete("/authors")
 def delete_author(id: int):
-
     if not id:
-        return JSONResponse(content={"error": "ID is required"}, status_code=400)
+        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
     
     try:
-        author = session.query(Author).filter(Author.id == id).first()
+        author = session.query(Author).filter_by(id=id).first()
+        if not author:
+            return JSONResponse(content={'error': 'Author not found'}, status_code=404)
         session.delete(author)
         session.commit()
     except Exception as e:
         session.rollback()
-        return JSONResponse(content={"error": str(e)}, status_code=500)
-
-    return JSONResponse(content={"id": author.id, "name": author.name}, status_code=200)
-
-@app.get("/author/{author_id}")  # Adicionei a barra inicial
-def get_author(author_id: int):
-    if not author_id:
-        return JSONResponse(content={"error": "ID is required"}, status_code=400)
+        return JSONResponse(content={'error': str(e)}, status_code=500)
     
-    try:
-        author = session.query(Author).filter(Author.id == author_id).first()
-    except Exception as e:
-        session.rollback()
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+    return JSONResponse(content={'message': 'Author deleted successfully'}, status_code=200)
 
-    if not author:
-        return JSONResponse(content={"error": "Author not found"}, status_code=404)
 
-    return JSONResponse(content={"id": author.id, "name": author.name}, status_code=200)
 
 @app.get('/authors/all')
 def get_all_authors():
@@ -118,3 +108,123 @@ def get_all_authors():
         return JSONResponse(content={'error': str(e)}, status_code=500)
     
     return JSONResponse(content=authors, status_code=200)
+
+
+@app.get('authors/{author_id}/')
+def get_author(author_id: int):
+    if not author_id:
+        return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+    
+    try:
+        author = session.query(Author).filter_by(id=author_id).first()
+        if not author:
+            return JSONResponse(content={'error': 'Author not found'}, status_code=404)
+    except Exception as e:
+        session.rollback()
+        return JSONResponse(content={'error': str(e)}, status_code=500)
+    
+    return JSONResponse(content={'id': author.id, 'name': author.name}, status_code=200)
+
+
+@app.post("/posts")
+def create_post(title: str, authorid: int):
+    
+    if not title:
+        return JSONResponse(content={'error': 'Title is required'}, status_code=400)   
+    if not authorid:
+        return JSONResponse(content={'error': 'Author ID is required'}, status_code=400) 
+
+    try:
+        author = session.query(Author).filter_by(id=authorid).first()
+
+        if not author:
+            return JSONResponse(content={'error': 'Author not found'}, status_code=404)
+        
+        post = Post(title=title, created= datetime.now(), authorid=authorid)
+        session.add(post)
+        session.commit()
+
+        return JSONResponse(content={'id': post.id, 'title': post.title, 'created': str(post.created), 'authorid': post.authorid}, status_code=201)
+    
+    except Exception as e:
+        session.rollback()
+        return JSONResponse(content={'error': str(e)}, status_code=500)
+
+
+
+@app.put("/posts")
+def put_post(id: int, title: str, authorid: int):
+    
+    try:
+        if not id:
+            return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+        if not title:
+            return JSONResponse(content={'error': 'Title is required'}, status_code=400)
+        if not authorid:
+            return JSONResponse(content={'error': 'Author ID is required'}, status_code=400)
+        
+        post = session .query(Post).filter_by(id=id).first()
+        if not post:
+            return JSONResponse(content={'error': 'Post not found'}, status_code=404)
+        
+        author = session.query(Author).filter_by(id=authorid).first()
+        if not author:
+            return JSONResponse(content={'error': 'Author not found'}, status_code=404)
+        
+        post.title = title
+        post.authorid = authorid
+        session.commit()
+
+        return JSONResponse(content={'id': post.id, 'title': post.title, 'created': str(post.created), 'authorid': post.authorid}, status_code=200)
+    
+    except Exception as e:
+        session.rollback()
+        return JSONResponse(content={'error': str(e)}, status_code=500)
+    
+@app.delete("/posts")
+def delete_post(id: int):
+
+    try:
+        if not id:
+            return JSONResponse(content={'error': 'ID is required'}, status_code=400)
+        
+        post = session.query(Post).filter_by(id=id).first()
+        if not post:
+            return JSONResponse(content={'error': 'Post not found'}, status_code=404)
+        
+        session.delete(post)
+        session.commit()
+    
+        return JSONResponse(content={'message': 'Post deleted successfully'}, status_code=200)
+    
+    except Exception as e:
+        session.rollback()
+        return JSONResponse(content={'error': str(e)}, status_code=500)
+
+
+@app.get("/posts")
+def get_posts():
+
+    try:        
+        posts = session.query(Post).all()
+        
+        post_list = []
+        
+        for post in posts:
+            
+            author = session.query(Author).filter_by(id=post.authorid).first()
+            if not author:
+                return JSONResponse(content={'error': 'Author not found'}, status_code=404)
+            
+            post_list.append({'id': post.id, 
+                              'title': post.title, 
+                              'created': str(post.created), 
+                              'authorid': author.id, 
+                              'author_name': author.name})
+
+        return JSONResponse(content=post_list, status_code=200)
+    
+    except Exception as e:
+        session.rollback()
+        return JSONResponse(content={'error': str(e)}, status_code=500)
+    
